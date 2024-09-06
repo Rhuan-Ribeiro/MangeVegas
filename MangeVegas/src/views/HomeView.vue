44f7bdba9d590fe74bbb7d3c19e2d490a5c2d084
123 <123@123.com>
2024-09-05 22:31:00 -0300

    start of new sections

--- MangeVegas/src/views/HomeView.vue
+++ MangeVegas/src/views/HomeView.vue
@@ -4,14 +4,57 @@
 
 <template>
   <main>
-    <h1>Home Page</h1>
+    <section id="intro">
+      <h1>Vicêncie a experiência definitiva de Cyberpunk 2077</h1>
+    </section>
+    <section id="news">
+      <img src="/razor.svg" style="height: auto;">
+    </section>
+    <section id="products">
+      
+      
+    </section>
+    
   </main>
 </template>
 
 <style scoped lang="scss">
- main {
+#intro {
   height: 100vh;
-  object-fit: cover;
   background-image: url(/poster.jpg);
- }
+  background-size: cover;   /* Ajusta o tamanho da imagem para cobrir o fundo */
+  background-position: center; /* Centraliza a imagem */
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+
+  h1 {
+    max-width: 587px;
+    margin-left: 50px;
+    line-height: 90%;
+    font-family: 'Refinery-25';
+    text-transform: uppercase;
+    font-size: 5rem;
+    font-weight: 700;
+    color: var(--black);
+  }
+}
+
+#news {
+  background-color: var(--black);
+  height: 500px;
+  position: relative;
+  img {
+    position: absolute;
+    bottom: 0;
+  }
+}
+
+#products {
+  background-color: var(--neon-yellow);
+  height: 500px;
+}
+
+
 </style>
